f050fbfd82f38cdc033c41e0b41ada2506337fc7
wuyanmiao <yanmiao.wu@tcl.com>
2023-11-20 18:41:05 +0800

    lettuce池化使用, -不建议使用

--- src/main/java/com/tech/skill/forge/controller/UserController.java
+++ src/main/java/com/tech/skill/forge/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.tech.skill.forge.controller;
 
+import cn.hutool.core.lang.UUID;
+import cn.hutool.core.util.RandomUtil;
 import com.tech.skill.forge.entity.User;
 import com.tech.skill.forge.service.db.UserSecureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author yanmiao.wu
@@ -20,6 +25,9 @@ public class UserController {
     @Resource
     UserSecureService userSecureService;
 
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @GetMapping("/register")
     public String register(@RequestParam("id") String accountId) {
         User user = new User();
@@ -28,6 +36,13 @@ public class UserController {
         user.setPassword("123456");
         user.setCreateTime(new Date());
         userSecureService.save(user);
+        return "";
+    }
+
+    @GetMapping("/redis")
+    public String testRedis() {
+        String s = RandomUtil.randomString(10);
+        redisTemplate.opsForValue().set(s,s,20, TimeUnit.SECONDS);
         return "";
     }
 
