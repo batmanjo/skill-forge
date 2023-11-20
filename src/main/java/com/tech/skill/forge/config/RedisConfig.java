9a5c84d0ede58279cdd4fc93b3f7fa123762a0d6
wuyanmiao <yanmiao.wu@tcl.com>
2023-11-20 16:40:32 +0800

    简单使用redis

---
+++ src/main/java/com/tech/skill/forge/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.tech.skill.forge.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+/**
+ * @author yanmiao.wu
+ * @create 2023-11-20 16:19
+ */
+@Configuration
+public class RedisConfig {
+
+    /**
+     * 生成通用redisTemplate
+     */
+    @Bean
+    public RedisTemplate<String, Object>  redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setValueSerializer(RedisSerializer.json());
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashValueSerializer(RedisSerializer.json());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
--- src/test/java/com/tech/skill/forge/SkillForgeApplicationTests.java
+++ src/test/java/com/tech/skill/forge/SkillForgeApplicationTests.java
@@ -1,10 +1,14 @@
 package com.tech.skill.forge;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.tech.skill.forge.entity.User;
 import com.tech.skill.forge.service.db.UserSecureService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.Date;
 import java.util.List;
@@ -15,6 +19,11 @@ class SkillForgeApplicationTests {
     @Autowired
     UserSecureService userSecureService;
 
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @Test
     void addUser() {
         User user = new User();
@@ -24,7 +33,6 @@ class SkillForgeApplicationTests {
         user.setCreateTime(new Date());
         userSecureService.save(user);
     }
-
     @Test
     void queryUser(){
         User userByAccountId = userSecureService.getUserByAccountId("3");
@@ -33,6 +41,19 @@ class SkillForgeApplicationTests {
         List<User> usersByTime = userSecureService.getUsersByTime(startTime, date);
         System.out.println(usersByTime);
         System.out.println(userByAccountId);
+    }
+    @Test
+    void setAndGetRedisUser(){
+        stringRedisTemplate.opsForValue().set("key1","hello");
+        String key1 = stringRedisTemplate.opsForValue().get("key1");
+        System.out.println(key1 + "world");
+
+        User userByAccountId = userSecureService.getUserByAccountId("3");
+        //如未配置redisTemplate,以下代码报错
+        redisTemplate.opsForValue().set("user3", userByAccountId);
+        User user = (User)redisTemplate.opsForValue().get("user3");
+        System.out.println(user);
+
     }
 
 }
